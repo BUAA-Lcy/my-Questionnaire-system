4823f92db5b81f47e513bdedaefb10e2e86d8276
Lvchaoyue <1417743380@qq.com>
2023-05-21 20:34:20 +0800

    modify all

--- src/components/Result/statistic.vue
+++ src/components/Result/statistic.vue
@@ -62,24 +62,57 @@
       <el-form ref="form" :model="form" label-width="120px" class="questionlist">
               <!-- 遍历问卷中的问题 -->
               <div v-for="question in state.questions" :key="question.id" class="question-item">
-              <h2>{{ question.title }}</h2>
-              <p class="question-description">{{ question.description }}</p>
-                  <div :id="'chart'+ question.id "  style="width: 600px;height:400px;"></div>
-                  <div class="button-container">
-                    <div v-if="question.questionType === 'choice'">
-                      <el-button class="rendering_button" v-for="chartType in chartTypes_for_chart" :key="chartType" @click="changeChartType(question.id, chartType, question)">
-                          {{ chartType }}
-                      </el-button>
-                    </div>
-                    <div v-if="question.questionType === 'text'">
-                      <el-button class="rendering_button" v-for="chartType in chartTypes_for_chart" :key="chartType" @click="changeChartType(question.id, chartType, question)">
-                          {{ chartType }}
-                      </el-button>
-                    </div>
-                    
-                 
+                <h2 class="question-title">{{ question.title }}</h2>
+                <div class="question-description">{{ question.description }}</div>
+
+                <div v-if="question.questionType === 'choice'">
+                  <div class="question-chart-container">
+                    <div :id="'chart'+ question.id "  class="question-chart"></div>
                   </div>
-                  
+                  <div class="button-container">
+                      <el-button class="rendering_button" v-for="chartType in chartTypes_for_chart" :key="chartType" @click="changeChartType(question.id, chartType, question)">
+                          {{ chartType }}
+                      </el-button>
+                  </div>
+                </div>
+                <div v-if="question.questionType === 'star'">
+                  <div class="question-chart-container">
+                    <div :id="'chart'+ question.id "  class="question-chart"></div>
+                  </div>
+                  <div class="button-container">
+                      <el-button class="rendering_button" v-for="chartType in chartTypes_for_star" :key="chartType" @click="changeChartType(question.id, chartType, question)">
+                          {{ chartType }}
+                      </el-button>
+                  </div>
+                </div>
+                <div v-if="question.questionType === 'number'">
+                  <div class="question-chart-container">
+                    <div :id="'chart'+ question.id "  class="question-chart"></div>
+                  </div>
+                  <div class="button-container">
+                      <el-button class="rendering_button" v-for="chartType in chartTypes_for_number" :key="chartType" @click="changeChartType(question.id, chartType, question)">
+                          {{ chartType }}
+                      </el-button>
+                  </div>
+                </div>
+                <div v-if="question.questionType === 'text'">
+                  <div class="table-container">
+                    <table >
+                    <thead> 
+                      <tr>
+                        <th>编号</th>
+                        <th>回答</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="(answer, index) in question.seriesData" :key="index">
+                        <td>{{ index + 1 }}</td>
+                        <td>{{ answer }}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  </div>
+                </div>
               </div>
         </el-form>
     </el-main>
@@ -104,7 +137,9 @@ const sumtableData = reactive([
 ]);
 const questionnaireTitle = ref('问卷标题');
 const activeIndex = ref('/');
-const chartTypes_for_chart = ['bar', 'line', 'pie','test'];
+const chartTypes_for_chart = ['bar', 'line', 'pie','ring'];
+const chartTypes_for_star = ['bar','line','pie','ring'];
+const chartTypes_for_number = ['scatter','ring','line'];
 
 const state = reactive({
     questions: [ 
@@ -138,6 +173,55 @@ const state = reactive({
     categories: ['选项1', '选项2', '选项3'],
     seriesData: [70, 80, 30],
   },
+  {
+    id: 4,
+    title: '问题4',
+    questionType: 'text',
+    chartType: 'pie',
+    stem: '这是问题4的题干',
+    description: '这是问题4的描述',
+    categories: ['选项1', '选项2', '选项3'],
+    seriesData: ["yodsaasddas", "adsasadsas", "adsas212"],
+  },
+  {
+    id: 5,
+    title: '问题5',
+    questionType: 'text',
+    chartType: 'pie',
+    stem: '这是问题5的题干',
+    description: '这是问题5的描述',
+    categories: ['选项1', '选项2', '选项3'],
+    seriesData: ["adsadsdasdasd", "fgdgdfdsasasdsadadsadsas", "sas2dasads12"],
+  },
+  {
+    id: 6,
+    title: '问题6',
+    questionType: 'star',
+    chartType: 'pie',
+    stem: '这是问题6的题干',
+    description: '这是问题6的描述',
+
+    //从1-10的自然数 
+    categories: [1,2,3,4,5,6,7,8,9,10],
+    //每个选项的人数
+    seriesData:  [6,5,6,7,8,1,2,4,6,8],
+  },
+  {
+    id: 7,
+    title: '问题7',
+    questionType: 'number',
+    chartType: 'pie',
+    stem: '这是问题7的题干',
+    description: '这是问题7的描述',
+
+    number_data: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+
+    // 这时候categories就表示每个选项选择的个数
+    categories:  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+
+    //每个选项的人数
+    seriesData:  [1,1,1,2,1,3,1,4,10,1,2,1,1,1,1],
+  },
    ],
   });
   
@@ -146,11 +230,20 @@ const state = reactive({
 
   onMounted(() => {
     state.questions.forEach((question) => {
-        chartRefs[question.id] = echarts.init(document.getElementById('chart'+question.id));        
+        if(question.questionType === 'choice'){
+          chartRefs[question.id] = echarts.init(document.getElementById('chart'+question.id));
+          renderChart(question.id, question.chartType, question);   
+        }   
+        if(question.questionType === 'star'){
+          chartRefs[question.id] = echarts.init(document.getElementById('chart'+question.id));
+          renderChart(question.id, question.chartType, question);   
+        }   
+        if(question.questionType === 'number'){
+          chartRefs[question.id] = echarts.init(document.getElementById('chart'+question.id));
+          renderChart(question.id, question.chartType, question);   
+        }  
         });
-    state.questions.forEach((question) => {
-        renderChart(question.id, question.chartType, question);
-      });
+    
       
     });
   
@@ -204,8 +297,8 @@ const state = reactive({
 
         
         };
-    }else if (chartType === 'test') {
-      var template = [];
+    }else if (chartType === 'ring') {
+        var template = [];
         for(let i=0;i<question.categories.length;i++){
             template.push({value:question.seriesData[i],name:question.categories[i]})
         }
@@ -222,7 +315,24 @@ const state = reactive({
             radius: ['40%', '70%']
             }
         ]
-};
+      };
+    }else if (chartType === 'scatter') {
+      // 注意，散点图纵坐标是值，而不是人数
+      var template = [];
+        for(let i=0;i<question.number_data.length;i++){
+            template.push([i,question.seriesData[i]])
+        }
+      option = {
+        xAxis: {},
+        yAxis: {},
+        series: [
+          {
+            symbolSize: 20,
+            data: template,
+            type: 'scatter'
+          }
+        ]
+      };
     }
     chartRefs[questionId].clear();
     chartRefs[questionId].setOption(option);
@@ -239,4 +349,5 @@ const state = reactive({
 <style src="./css/overview.css"></style>
 <style src="./css/question.css"></style>
 <style src="./css/button.css"></style>
+<style src="./css/questionTable.css"></style>
 
